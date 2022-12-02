c61a7b232a1a9e1c6226726a75003604ac801925
LeKhoa <khoaltdps20142@fpt.edu.vn>
2022-12-02 10:32:57 +0700

    cho du lieu len bang nhan vien

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 //    double x,y = 0;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../FXML_File/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../FXML_File/Employees.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("../Css/Login.css").toExternalForm());
         primaryStage.initStyle(StageStyle.DECORATED.UNDECORATED);
